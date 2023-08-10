cae988564dac437e9e556b5ff06b0ae1a188fe50
Kennedy Musau <kennedy.musauu@gmail.com>
2023-08-10 22:13:37 +0300

    add routes

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -1,49 +1,49 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Students", type: :request do
   before do
-    Student.create(first_name: 'Dwayne', last_name: 'Johnson', grade: 99)
-    Student.create(first_name: 'Idris', last_name: 'Elba', grade: 105)
-    Student.create(first_name: 'Vanessa', last_name: 'Kirby', grade: 85)
-    Student.create(first_name: 'Jason', last_name: 'Statham', grade: 70)
+    Student.create(first_name: "Dwayne", last_name: "Johnson", grade: 99)
+    Student.create(first_name: "Idris", last_name: "Elba", grade: 105)
+    Student.create(first_name: "Vanessa", last_name: "Kirby", grade: 85)
+    Student.create(first_name: "Jason", last_name: "Statham", grade: 70)
   end
 
   let!(:first_student) { Student.first }
   let!(:second_student) { Student.second }
 
   describe "GET /students/:id" do
-    it 'returns the first student' do
+    it "returns the first student" do
       get "/students/#{first_student.id}"
 
       expect(response.body).to include_json({
         id: a_kind_of(Integer),
-        first_name: 'Dwayne', 
-        last_name: 'Johnson', 
-        grade: 99
+        first_name: "Dwayne",
+        last_name: "Johnson",
+        grade: 99,
       })
     end
 
-    it 'returns the second student' do
+    it "returns the second student" do
       get "/students/#{second_student.id}"
 
       expect(response.body).to include_json({
         id: a_kind_of(Integer),
-        first_name: 'Idris', 
-        last_name: 'Elba', 
-        grade: 105
+        first_name: "Idris",
+        last_name: "Elba",
+        grade: 105,
       })
     end
   end
 
   describe "GET /students" do
-    it 'returns an array of all students' do
-      get '/students'
+    it "returns an array of all students" do
+      get "/students"
 
       expect(response.body).to include_json([
-        { first_name: 'Dwayne', last_name: 'Johnson', grade: 99 },
-        { first_name: 'Idris', last_name: 'Elba', grade: 105 },
-        { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 },
-        { first_name: 'Jason', last_name: 'Statham', grade: 70 }
+        { first_name: "Dwayne", last_name: "Johnson", grade: 99 },
+        { first_name: "Idris", last_name: "Elba", grade: 105 },
+        { first_name: "Vanessa", last_name: "Kirby", grade: 85 },
+        { first_name: "Jason", last_name: "Statham", grade: 70 },
       ])
     end
 
@@ -58,7 +58,6 @@ RSpec.describe "Students", type: :request do
     #     ])
     #   end
     # end
-    
+
   end
-  
 end
